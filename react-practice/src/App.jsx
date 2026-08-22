40737cac3d3814ec388e4fa33f85b3aef66ea87f
gitanshuchavda <gitanshudz@gmail.com>
2026-08-22 11:20:13 +0530

    made counter using react

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css"
+
+function App(){
+    const [count, setCount] = useState(0);
+    return(
+        <div className="counter">
+            <h1>{count} </h1>
+            <div className="buttons">
+              <button onClick={() => setCount(count+1)}>+</button>
+             <button onClick={() => setCount(count - 1)}>-</button>
+             <button onClick={() => setCount(0)}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
